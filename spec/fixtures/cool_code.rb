664256946b95ebe1469d5923157413a12374614c
Michael Bernstein <michaelrbernstein@gmail.com>
2015-11-17 12:17:41 -0500

    Add spec

---
+++ spec/fixtures/cool_code.rb
@@ -0,0 +1,51 @@
+class CoolCode
+  def foo (bar="bip", baz, bounce)
+    baz = 1
+    bounce = 3
+  end
+
+  def long_one
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+    lambda{}
+  end
+end
