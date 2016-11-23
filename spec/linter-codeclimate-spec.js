9434a6fd20a68aae60e35e2bd573094a3a9d592c
Landon Abney <Landonabney@gmail.com>
2016-11-23 15:31:05 -0800

    Rewrite in ES2017
    Rewrite the linter in ES2017 code, including the following changes:
    
    * Use Helpers.rangeFromLineNumber() to generate ranges for incomplete
      positions, this will use the regex for the current language to highlight
      the next "word" automatically.
    * Move to the Performance API for measuring time. Times are only logged to
      the console in Developer mode now.
    * Move the setting to a `configSchema` in the `package.json` so Atom
      doesn't have to execute the code to read the settings.
    * Utilize `async`/`await` where possible

---
+++ spec/linter-codeclimate-spec.js
@@ -0,0 +1,34 @@
+'use babel';
+
+import { join } from 'path';
+import helpers from 'atom-linter';
+
+const fixturesPath = join(__dirname, 'fixtures');
+const coolCodePath = join(fixturesPath, 'cool_code.rb');
+
+describe('linter-codeclimate package', () => {
+  beforeEach(() => {
+    waitsForPromise(() =>
+      Promise.all([
+        atom.packages.activatePackage('language-ruby'),
+        atom.packages.activatePackage('linter'),
+        atom.packages.activatePackage('linter-codeclimate'),
+      ]).then(() =>
+        atom.workspace.open(coolCodePath),
+      ),
+    );
+    spyOn(helpers, 'exec');
+  });
+
+  describe('with a valid .codeclimate.yml file', () =>
+    it('runs codeclimate-linter on save', () => {
+      const cmd = "/usr/local/bin/codeclimate analyze -f json -e fixme -e rubocop 'cool_code.rb' < /dev/null";
+      expect(atom.workspace.getActiveTextEditor().getTitle()).toBe('cool_code.rb');
+      expect(atom.workspace.getActiveTextEditor().getGrammar().name).toBe('Ruby');
+      runs(() => {
+        atom.commands.dispatch(atom.views.getView(atom.workspace), 'core:save');
+        expect(helpers.exec).toHaveBeenCalledWith('/bin/bash', ['-lc', cmd], { cwd: fixturesPath });
+      });
+    }),
+  );
+});
