0fb5c56385ebc5de1276b4414ddf83af24e33aac
Landon Abney <Landonabney@gmail.com>
2017-02-15 11:11:35 -0800

    Re-write specs
    The "full functional" tests used before have proved to be far too
    problematic in other providers, and weren't working at all here.
    Re-write the specs to just test this providers, instead of testing parts
    of Atom and Linter.

--- spec/linter-codeclimate-spec.js
+++ spec/linter-codeclimate-spec.js
@@ -2,34 +2,34 @@
 
 import { join } from 'path';
 
-const helpers = require('atom-linter');
-
 const fixturesPath = join(__dirname, 'fixtures');
 const coolCodePath = join(fixturesPath, 'cool_code.rb');
 
-describe('linter-codeclimate package', () => {
+describe('The codeclimate provider for Linter', () => {
+  const lint = require('../lib/index.js').provideLinter().lint;
+
   beforeEach(() => {
+    atom.workspace.destroyActivePaneItem();
+
     waitsForPromise(() =>
       Promise.all([
         atom.packages.activatePackage('language-ruby'),
-        atom.packages.activatePackage('linter'),
         atom.packages.activatePackage('linter-codeclimate'),
-      ]).then(() =>
-        atom.workspace.open(coolCodePath),
-      ),
+      ]),
     );
-    spyOn(helpers, 'exec');
   });
 
-  describe('with a valid .codeclimate.yml file', () =>
-    it('runs codeclimate-linter on save', () => {
-      const cmd = "/usr/local/bin/codeclimate analyze -f json -e fixme -e rubocop 'cool_code.rb' < /dev/null";
-      expect(atom.workspace.getActiveTextEditor().getTitle()).toBe('cool_code.rb');
-      expect(atom.workspace.getActiveTextEditor().getGrammar().name).toBe('Ruby');
-      runs(() => {
-        atom.commands.dispatch(atom.views.getView(atom.workspace), 'core:save');
-        expect(helpers.exec).toHaveBeenCalledWith('/bin/bash', ['-lc', cmd], { cwd: fixturesPath });
-      });
-    }),
+  it('works with a valid .codeclimate.yml file', () =>
+    waitsForPromise(() =>
+      atom.workspace.open(coolCodePath).then(editor => lint(editor)).then(
+        (messages) => {
+          expect(messages[0].type).toBe('Warning');
+          expect(messages[0].text).toBe('some text');
+          expect(messages[0].html).not.toBeDefined();
+          expect(messages[0].filePath).toBe(coolCodePath);
+          expect(messages[0].range).toEqual([[0, 5], [0, 7]]);
+        },
+      ),
+    ),
   );
 });
