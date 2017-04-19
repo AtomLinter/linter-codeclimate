d0d37803c45ece910e3ca60585ab8da299cd6674
Carlos García <cgalvarez.engineer@gmail.com>
2017-04-19 18:10:16 +0200

    fix duplicated issues and upgrade to linter v2

--- spec/linter-codeclimate-spec.js
+++ spec/linter-codeclimate-spec.js
@@ -23,14 +23,18 @@ describe('The codeclimate provider for Linter', () => {
     waitsForPromise(() =>
       atom.workspace.open(coolCodePath).then(editor => lint(editor)).then(
         (messages) => {
-          expect(messages[0].type).toBe('Warning');
-          expect(messages[0].text).toBe('Unused method argument - `bar`. If ' +
-            "it's necessary, use `_` or `_bar` as an argument name to indicate " +
-            "that it won't be used. You can also write as `foo(*)` if you want " +
-            "the method to accept any arguments but don't care about them.");
-          expect(messages[0].html).not.toBeDefined();
-          expect(messages[0].filePath).toBe(coolCodePath);
-          expect(messages[0].range).toEqual([[1, 11], [1, 14]]);
+          expect(messages[0].severity).toBe('warning');
+          expect(messages[0].excerpt).toBe('RUBOCOP: Unused method argument - ' +
+            "`bar`. If it's necessary, use `_` or `_bar` as an argument name to " +
+            "indicate that it won't be used. You can also write as `foo(*)` if " +
+            "you want the method to accept any arguments but don't care about " +
+            'them. [Rubocop/Lint/UnusedMethodArgument]');
+          expect(messages[0].description).toBeDefined();
+          expect(messages[0].reference).not.toBeDefined();
+          expect(messages[0].icon).not.toBeDefined();
+          expect(messages[0].solutions).not.toBeDefined();
+          expect(messages[0].location.file).toBe(coolCodePath);
+          expect(messages[0].location.position).toEqual([[1, 11], [1, 14]]);
         },
       ),
     ),
