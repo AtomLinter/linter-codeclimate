9f645adc2f324203502354cc49e947682ba96930
Alexander Mankuta <alex@pointless.one>
2017-05-11 15:17:51 +0300

    Add extended timeout for CI

--- spec/linter-codeclimate-spec.js
+++ spec/linter-codeclimate-spec.js
@@ -4,6 +4,7 @@ import { join } from 'path';
 
 const fixturesPath = join(__dirname, 'fixtures');
 const coolCodePath = join(fixturesPath, 'cool_code.rb');
+const TIMEOUT = process.env.CI ? 60000 : 10000;
 
 describe('The codeclimate provider for Linter', () => {
   const lint = require('../lib/index.js').provideLinter().lint;
@@ -20,7 +21,7 @@ describe('The codeclimate provider for Linter', () => {
 
   it('works with a valid .codeclimate.yml file', () =>
     waitsForPromise(
-      { timeout: 10000 },
+      { timeout: TIMEOUT },
       () =>
       atom.workspace.open(coolCodePath).then(editor => lint(editor)).then(
         (messages) => {
