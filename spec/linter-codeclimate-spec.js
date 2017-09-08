98879de3af236e4f98ed4172cb2d47dd844c8145
Landon Abney <Landonabney@gmail.com>
2017-09-08 14:52:20 -0700

    Asyncify the specs
    Bring in `jasmine-fix` to allow the use of `async`/`await` in the specs
    and refactor them to take advantage of this.

--- spec/linter-codeclimate-spec.js
+++ spec/linter-codeclimate-spec.js
@@ -1,40 +1,36 @@
 'use babel';
 
+// eslint-disable-next-line no-unused-vars
+import { it, fit, wait, beforeEach, afterEach } from 'jasmine-fix';
 import { join } from 'path';
+
+const { lint } = require('../lib/index.js').provideLinter();
 
 const fixturesPath = join(__dirname, 'fixtures');
 const coolCodePath = join(fixturesPath, 'cool_code.rb');
-const TIMEOUT = process.env.CI ? 60000 : 10000;
 
 describe('The codeclimate provider for Linter', () => {
-  const { lint } = require('../lib/index.js').provideLinter();
-
-  beforeEach(() => {
+  beforeEach(async () => {
     atom.workspace.destroyActivePaneItem();
-
-    waitsForPromise(() =>
-      Promise.all([
-        atom.packages.activatePackage('linter-codeclimate'),
-      ]));
+    await atom.packages.activatePackage('linter-codeclimate');
   });
 
-  it('works with a valid .codeclimate.yml file', () =>
-    waitsForPromise(
-      { timeout: TIMEOUT },
-      () =>
-        atom.workspace.open(coolCodePath).then(editor => lint(editor)).then((messages) => {
-          expect(messages[0].severity).toBe('warning');
-          expect(messages[0].excerpt).toBe('RUBOCOP: Unused method argument - ' +
-            "`bar`. If it's necessary, use `_` or `_bar` as an argument name to " +
-            "indicate that it won't be used. You can also write as `foo(*)` if " +
-            "you want the method to accept any arguments but don't care about " +
-            'them. [Rubocop/Lint/UnusedMethodArgument]');
-          expect(messages[0].description).toBeDefined();
-          expect(messages[0].reference).not.toBeDefined();
-          expect(messages[0].icon).not.toBeDefined();
-          expect(messages[0].solutions).not.toBeDefined();
-          expect(messages[0].location.file).toBe(coolCodePath);
-          expect(messages[0].location.position).toEqual([[1, 11], [1, 14]]);
-        }),
-    ));
+  it('works with a valid .codeclimate.yml file', async () => {
+    const editor = await atom.workspace.open(coolCodePath);
+    const messages = await lint(editor);
+
+    expect(messages.length).toBe(1);
+    expect(messages[0].severity).toBe('warning');
+    expect(messages[0].excerpt).toBe('RUBOCOP: Unused method argument - ' +
+      "`bar`. If it's necessary, use `_` or `_bar` as an argument name to " +
+      "indicate that it won't be used. You can also write as `foo(*)` if " +
+      "you want the method to accept any arguments but don't care about " +
+      'them. [Rubocop/Lint/UnusedMethodArgument]');
+    expect(messages[0].description).toBeDefined();
+    expect(messages[0].reference).not.toBeDefined();
+    expect(messages[0].icon).not.toBeDefined();
+    expect(messages[0].solutions).not.toBeDefined();
+    expect(messages[0].location.file).toBe(coolCodePath);
+    expect(messages[0].location.position).toEqual([[1, 11], [1, 14]]);
+  });
 });
